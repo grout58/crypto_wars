ce75a74509e6d4bfa4b7b5fcca7a603dc084a7a0
grout58 <grout58@gmail.com>
2018-03-28 19:34:44 +0000

    more logic to objects

--- crypto_wars_objects.py
+++ crypto_wars_objects.py
@@ -17,32 +17,40 @@ class Market:
 
 
 class Crypto_Currencies:
-    def __init__(self, name, price, multi, rand_multi, low):
+    def __init__(self, name, multi, low, price=None, rand_multi=None):
         self.name = name
-        self.price = price
         self.multi = multi
-        self.rand_multi = rand_multi
         self.low = low
-
-
-    def set_price(self):
         self.rand_multi = self.multi * random.randint(1, self.multi)
         self.price = self.low * self.rand_multi
-        return self.price
+       
+
+
+## wallet
+class Wallet:
+    def __init__(self, wallet=[]):
+        self.wallet = wallet
+
+
+    def show_wallet(self):
+        for items in self.wallet:
+            print(items.name, items.price)
 
 
 ## inventory
 class Portfolio:
-    def __init__(self, crypto_name, purchased_price, current_value, ammount):
+    def __init__(self, crypto_name, purchased_price, current_value, ammount, wallet = []):
         self.crypto_name = crypto_name
         self.purchased_price = purchased_price
         self.current_value = current_value
         self.ammount = ammount
+        self.wallet = wallet
 
-    def purchase():
+
+    def purchase(self):
         pass
 
-
+    
 
 
 ## sales
